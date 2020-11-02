771df0aa4a26ca20aaf8bf453ab098ad318a5797
kaiorr <kaio.rocharibeiro@gmail.com>
2020-11-02 17:26:18 -0300

    feat: connection database solved

--- restaurante/routes/users.js
+++ restaurante/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var connection = require('./../utils/connection_database')
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  connection.query("SELECT * FROM tb_users ORDER BY NAME", (err, results) => {
+    if(err) {
+      res.send(err)
+    } else {
+      res.send(results)
+    }
+  })
 });
 
 module.exports = router;
